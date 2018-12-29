6fa52bf92b3774624a1fc42e6336246e1e38bcb1
ProblemSPb <ussr83@mail.ru>
2018-12-30 00:40:43 +0100

    handling the membership files (reader, (over)writer)

---
+++ src/FileHandler.java
@@ -0,0 +1,52 @@
+// handling the membership files
+
+import java.io.*;
+import java.util.LinkedList;
+
+public class FileHandler {
+
+    // method readFile -> reading from the csv files
+    public LinkedList<Member> readFile() {
+
+        LinkedList<Member> m = new LinkedList();
+        String lineRead;
+        String[] splitLine;
+        Member mem;
+
+        try (BufferedReader reader = new BufferedReader(new FileReader("members.csv"))) {
+            lineRead = reader.readLine();
+            while (lineRead != null) {
+                splitLine = lineRead.split(", "); // splitting the line into an array
+
+                if (splitLine[0].equals("S")) {         // if its a Single member, instantiate a SinleClassMember
+                    mem = new SingleClassMember('S', Integer.parseInt(splitLine[1]),
+                            splitLine[2], Double.parseDouble(splitLine[3]), Integer.parseInt(splitLine[4]));
+                } else {      // Multi Club member Object
+                    mem = new MultiClubMember('M', Integer.parseInt(splitLine[1]), splitLine[2],
+                            Double.parseDouble(splitLine[3]), Integer.parseInt(splitLine[4]));
+                }
+                m.add(mem);     // adding a newly created member to the List
+                lineRead = reader.readLine();
+            }
+        }
+
+        catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+
+        return m;
+    }
+
+
+    // method => appending new members to the file
+    public void appendFile(String mem) {
+
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter("members.csv", true))) {
+            writer.write(mem + "\n"); // \n will move the cursor to the next line
+        }
+        catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+}
