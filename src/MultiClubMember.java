580be247f31d1d0f6c6a12de2737538ea92f733c
ProblemSPb <ussr83@mail.ru>
2018-12-30 00:41:08 +0100

    subclass of Member, multi club membership

--- src/MultiClubMember.java
+++ src/MultiClubMember.java
@@ -1,5 +1,27 @@
 // subclass of Member
 // multi club membership
 
-public class MultiClubMember {
+public class MultiClubMember extends Member{
+
+    private int membershipPoints;
+
+    // constructor
+    MultiClubMember (char pMemberType, int pMemberID, String pName, double pFees, int pMembershipPoints) {
+        super(pMemberType, pMemberID, pName, pFees);
+        membershipPoints = pMembershipPoints;
+    }
+
+    // getter and setter
+    public int getMembershipPoints() {
+        return membershipPoints;
+    }
+
+    public void setMembershipPoints(int pMembershipPoints) {
+        membershipPoints = pMembershipPoints;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + ", " + membershipPoints;
+    }
 }
